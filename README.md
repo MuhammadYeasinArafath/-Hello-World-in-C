41387390b3de898375d005c1190c327f9019de8d
MuhammadYeasinArafath <mdyeasinarafathofficial@gmail.com>
2022-08-19 02:42:42 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# -Hello-World-in-C-
+“Hello, World!” in C
+<br>
+1st time programming in C programming language.
+<br>
---
+++ main.c
@@ -0,0 +1,11 @@
+//print (Hello, World!) in C.
+#include <stdio.h>
+int main() {
+   printf("Hello, world!");
+   return 0;
+}
+
+
+//applicable in this file only
+//type {g++ ./main.c} to debug in terminal.
+//type {./a.exe} to run the code.
